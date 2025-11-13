b71395b59049a26d93d183bc30e6702dcaee0702
Denis Dreano <denis.dreano@protonmail.ch>
2025-11-13 13:16:17 +0100

    cours 2

---
+++ 2_web/js/exemple.js
@@ -0,0 +1,25 @@
+var message = 'Bienvenue';
+
+// console.log(message);
+
+// function ma_fonction() {
+//     console.log('ma fonction');
+// }
+
+// ma_fonction();
+
+// console.log(document.getElementById('bienvenue').textContent)
+
+// for (var i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+
+function ajouter_perso() {
+    var nom_perso = 'Jean';
+
+    var nouveau_li = document.createElement('li');
+    var nouveau_text = document.createTextNode(text_personnage);
+    
+    console.log('perso');
+}
+
